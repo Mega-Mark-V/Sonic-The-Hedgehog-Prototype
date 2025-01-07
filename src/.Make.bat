dc8f07f4695b1748e9ef7d16f69274e2f667d3f5
MDTravis <.>
2025-01-07 01:47:33 -0500

    fix up build script executable calls so it can run in windows

--- src/.Make.bat
+++ src/.Make.bat
@@ -1,6 +1,6 @@
 @echo off
-set ASM68K=_bin/asm68k.exe /p /o ae-,l.,ow+
-set VASM=_bin/vasmm68k_psi-x.exe -altlocal -m68000 -maxerrors=0 -Fbin -start=0
+set ASM68K="_bin/asm68k.exe" /p /o ae-,l.,ow+
+set VASM="_bin/vasmm68k_psi-x.exe" -altlocal -m68000 -maxerrors=0 -Fbin -start=0
 
 if not exist _built mkdir _built
 %VASM% -o "_built/_ROM.MDRV" -L "_built/_ROM.LST" -Lall "MAIN.68k" 2> _built/_ROM.log
