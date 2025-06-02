7bb7def51c9041cb5f89ce1c821033707b793e39
kernel_of_silver <101675887+kernel-of-silver@users.noreply.github.com>
2025-06-02 13:13:15 -0500

    Basic split for enemy Piranha

---
+++ src/Objects/Enemy/Piranha/PIRANHA_ANI.asm
@@ -0,0 +1,8 @@
+.anitbl:
+	dc.w	byte_8C42-.anitbl
+	dc.w	byte_8C46-.anitbl
+	dc.w	byte_8C4A-.anitbl
+
+byte_8C42:	dc.b	7,0,1,$FF
+byte_8C46:	dc.b	3,0,1,$FF
+byte_8C4A:	dc.b	7,0,$FF,0
---
+++ src/Objects/Enemy/Piranha/PIRANHA_PAT.asm
@@ -0,0 +1,6 @@
+.pattbl:
+	dc.w	byte_8C52-.pattbl
+	dc.w	byte_8C58-.pattbl
+
+byte_8C52:	dc.b	1,$F0,$F,0,0,$F0
+byte_8C58:	dc.b	1,$F0,$F,0,$10,$F0
