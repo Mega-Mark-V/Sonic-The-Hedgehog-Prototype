e630ad72dca1f1f169cad7103fa0f4a10f70f56e
kernel_of_silver <101675887+kernel-of-silver@users.noreply.github.com>
2025-06-04 04:33:43 -0500

    Clean up, update database Bluntly I forgot to sync most of the things done in src/ with the database. Sorry

--- src/Objects/Enemy/Chameleon/CHAMELEON_PAT.asm
+++ src/Objects/Enemy/Chameleon/CHAMELEON_PAT.asm
@@ -11,25 +11,25 @@
 	dc.w cham_spr09-.pattbl
 	dc.w cham_spr0A-.pattbl
 
-cham_spr00:	dc.b	3,$EC, $D,  0,  0,$EC,$F4,  0 
-		dc.b	0,  8, $C,$FC, $E,  0,  9,$F4
-cham_spr01:	dc.b	3,$EC,  6,  0,$15,$EC,$EC,  9 
-		dc.b	0,$1B,$FC,$FC, $A,  0,$21,$FC
-cham_spr02:	dc.b	3,$EC,  6,  0,$2A,$EC,$EC,  9 
-		dc.b	0,$1B,$FC,$FC, $A,  0,$21,$FC
-cham_spr03:	dc.b	4,$EC,  6,  0,$30,$EC,$EC,  9 
-		dc.b	0,$1B,$FC,$FC,  9,  0,$36,$FC
-		dc.b	$C,  0,  0,$3C, $C
-cham_spr04:	dc.b	3,$F4, $D,  0,$3D,$EC,$FC,  0 
-		dc.b	0,$20, $C,  4,  8,  0,$45,$FC
-cham_spr05:	dc.b	2,$F8, $D,  0,$48,$EC,$F8,  1 
-		dc.b	0,$50, $C
-cham_spr06:	dc.b	3,$F8, $D,  0,$48,$EC,$F8,  1 
-		dc.b	0,$50, $C,$FE,  0,  0,$52,$14
-cham_spr07:	dc.b	3,$F8, $D,  0,$48,$EC,$F8,  1 
-		dc.b	0,$50, $C,$FE,  4,  0,$53,$14
-cham_spr08:	dc.b	3,$F8, $D,  0,$48,$EC,$F8,  1 
-		dc.b	0,$50, $C,$FE,  0,$E0,$52,$14
-cham_spr09:	dc.b	3,$F8, $D,  0,$48,$EC,$F8,  1 
-		dc.b	0,$50, $C,$FE,  4,$E0,$53,$14
-cham_spr0A:	dc.w	0 
+cham_spr00:	dc.b   3,$EC, $D,  0,  0,$EC,$F4,  0 
+		dc.b   0,  8, $C,$FC, $E,  0,  9,$F4
+cham_spr01:	dc.b   3,$EC,  6,  0,$15,$EC,$EC,  9 
+		dc.b   0,$1B,$FC,$FC, $A,  0,$21,$FC
+cham_spr02:	dc.b   3,$EC,  6,  0,$2A,$EC,$EC,  9 
+		dc.b   0,$1B,$FC,$FC, $A,  0,$21,$FC
+cham_spr03:	dc.b   4,$EC,  6,  0,$30,$EC,$EC,  9 
+		dc.b   0,$1B,$FC,$FC,  9,  0,$36,$FC
+		dc.b  $C,  0,  0,$3C, $C
+cham_spr04:	dc.b   3,$F4, $D,  0,$3D,$EC,$FC,  0 
+		dc.b   0,$20, $C,  4,  8,  0,$45,$FC
+cham_spr05:	dc.b   2,$F8, $D,  0,$48,$EC,$F8,  1 
+		dc.b   0,$50, $C
+cham_spr06:	dc.b   3,$F8, $D,  0,$48,$EC,$F8,  1 
+		dc.b   0,$50, $C,$FE,  0,  0,$52,$14
+cham_spr07:	dc.b   3,$F8, $D,  0,$48,$EC,$F8,  1 
+		dc.b   0,$50, $C,$FE,  4,  0,$53,$14
+cham_spr08:	dc.b   3,$F8, $D,  0,$48,$EC,$F8,  1 
+		dc.b   0,$50, $C,$FE,  0,$E0,$52,$14
+cham_spr09:	dc.b   3,$F8, $D,  0,$48,$EC,$F8,  1 
+		dc.b   0,$50, $C,$FE,  4,$E0,$53,$14
+cham_spr0A:	dc.w   0 
--- src/Objects/Enemy/Piranha/PIRANHA_ANI.asm
+++ src/Objects/Enemy/Piranha/PIRANHA_ANI.asm
@@ -5,4 +5,5 @@
 
 pirachompslow:	dc.b	7,0,1,$FF
 pirachompfast:	dc.b	3,0,1,$FF
-pirastopchomp:	dc.b	7,0,$FF,0
+pirastopchomp:	dc.b	7,0,$FF
+		even
